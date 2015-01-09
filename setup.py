9e05de16a826e7f6c8dc138593073fac8cd69e1b
kaleissin <kaleissin@gmail.com>
2015-01-09 11:00:40 +0100

    Better trove-classifiers

--- setup.py
+++ setup.py
@@ -28,6 +28,8 @@ setup(name='django-multipageforms',
                 'License :: OSI Approved :: MIT License',
                 'Operating System :: OS Independent',
                 'Programming Language :: Python',
+                'Programming Language :: Python :: 2',
+                'Programming Language :: Python :: 2.7',
                 'Topic :: Software Development :: Libraries :: Application Frameworks',
                 'Topic :: Software Development :: Libraries :: Python Modules',
         ]
