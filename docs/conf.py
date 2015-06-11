b6a68717e5f177467774dd33c773b754b4de5bde
kaleissin <kaleissin@gmail.com>
2015-06-11 15:19:10 +0200

    Gotta fix the skeleton I use, ayup

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u''
-copyright = u'2014, YOURNAME'
+copyright = u'2014-2015, kaleissin'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
